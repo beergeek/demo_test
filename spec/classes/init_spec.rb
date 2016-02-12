b65cd6ed644eb852346cbd7060570bb52e4348b5
beergeek <brett.gray@puppetlabs.com>
2016-02-13 00:34:39 +1100

    initial commit

---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+describe 'demo_test' do
+
+  context 'with defaults for all parameters' do
+    it { should contain_class('demo_test') }
+
+    it {
+      should contain_user('demo').with(
+        'ensure'  => 'present',
+        'gid'     => 'demo',
+      )
+    }
+
+    it {
+      should contain_group('demo').with(
+        'ensure'  => 'present',
+      )
+    }
+
+    it {
+      should contain_file('/home/demo').with(
+        'ensure'  => 'directory',
+        'owner'   => 'demo',
+        'group'   => 'demo',
+        'mode'    => '0600',
+      )
+    }
+
+    it {
+      should contain_file('/home/demo/.ssh').with(
+        'ensure'  => 'directory',
+        'owner'   => 'demo',
+        'group'   => 'demo',
+        'mode'    => '0600',
+      )
+    }
+  end
+end
