8c21d167febacc5dfba3906a1eb558d9e8be8d25
beergeek <brett.gray@puppetlabs.com>
2016-02-16 14:36:03 +1100

    testing

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source 'https://rubygems.org'
 
 group :test do
-  puppetversion = ENV.key?('PUPPET_VERSION') ? "#{ENV['PUPPET_VERSION']}" : ['>= 3.3']
+  puppetversion = ENV.key?('PUPPET_VERSION') ? "#{ENV['PUPPET_VERSION']}" : ['>= 4.0']
   gem 'puppet', puppetversion
   gem 'puppetlabs_spec_helper', '>= 0.8.2'
   gem 'puppet-lint', '>= 1.0.0'
+  gem "metadata-json-lint"
   gem 'facter', '>= 1.7.0'
-  gem 'simplecov', '>= 0.11.0'
+  gem 'simplecov', :require => false, :group => :test
   gem 'simplecov-console'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,19 @@
 require 'simplecov'
 require 'simplecov-console'
 
-SimpleCov.config do
+SimpleCov.start do
   add_filter '/spec'
-  add_filter '/vendor'
+  add_filter '/files'
+  add_filter '/tests'
+  add_filter '/examples'
   formatter SimpleCov::Formatter::MultiFormatter.new([
     SimpleCov::Formatter::HTMLFormatter,
     SimpleCov::Formatter::Console
   ])
 end
-
-SimpleCov.start
+require 'puppetlabs_spec_helper/rake_tasks'
+require 'puppet-lint/tasks/puppet-lint'
+require 'puppet-syntax/tasks/puppet-syntax'
+require 'metadata-json-lint/rake_task'
 require 'puppetlabs_spec_helper/module_spec_helper'
+at_exit { RSpec::Puppet::Coverage.report! }
