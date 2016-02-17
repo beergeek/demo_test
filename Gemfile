4e860e984f275ebfd0aa71e368dbaa3b88f18f57
beergeek <brett.gray@puppetlabs.com>
2016-02-17 12:17:57 +1100

    added ext for code coverage

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 group :test do
   puppetversion = ENV.key?('PUPPET_VERSION') ? "#{ENV['PUPPET_VERSION']}" : ['>= 4.0']
   gem 'puppet', puppetversion
+  gem 'rspec-puppet', :git => 'http://10.20.1.125:7990/scm/poop/rspec-puppet.git'
   gem 'puppetlabs_spec_helper', '>= 0.8.2'
   gem 'puppet-lint', '>= 1.0.0'
   gem "metadata-json-lint"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,9 @@ require 'simplecov'
 require 'simplecov-console'
 
 SimpleCov.start do
-  #add_filter '/files'
-  #add_filter '/tests'
-  #add_filter '/examples'
+  add_filter '/files'
+  add_filter '/tests'
+  add_filter '/examples'
   formatter SimpleCov::Formatter::MultiFormatter.new([
     SimpleCov::Formatter::HTMLFormatter,
     SimpleCov::Formatter::Console
@@ -15,4 +15,4 @@ require 'puppet-lint/tasks/puppet-lint'
 require 'puppet-syntax/tasks/puppet-syntax'
 require 'metadata-json-lint/rake_task'
 require 'puppetlabs_spec_helper/module_spec_helper'
-at_exit { RSpec::Puppet::Coverage.report! }
+at_exit { RSpec::Puppet::Coverage.report!(90) }
